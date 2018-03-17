92d27b95b69f300eab07b30a6615373e6194d6b5
Jorge Diaz <jdiaz216@gmail.com>
2018-03-17 17:20:34 -0500

    Initial commit

---
+++ src/main/java/springfive/cms/domain/models/Category.java
@@ -0,0 +1,11 @@
+package springfive.cms.domain.models;
+
+import lombok.Data;
+
+@Data
+public class Category {
+
+    String id;
+
+    String name;
+}
---
+++ src/main/java/springfive/cms/domain/models/Tag.java
@@ -0,0 +1,9 @@
+package springfive.cms.domain.models;
+
+import lombok.Data;
+
+@Data
+public class Tag {
+
+    String value;
+}
